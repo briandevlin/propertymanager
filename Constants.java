68e9a850d57ff1b44529984eee5da03b43962dc1
briandevlin <brian.devlin@gmail.com>
2013-11-25 17:27:10 -0500

    first code file

---
+++ Constants.java
@@ -0,0 +1,25 @@
+package com.bdevlin.android.apps.propertymanager;
+
+public class Constants {
+
+	public static final String APPLICATION = "PropertyManager";
+
+	public static final String ACCOUNT_TYPE = "com.devlin.pmos.pmossync";
+
+	/**
+	 * Authtoken type string.
+	 */
+	public static final String AUTHTOKEN_TYPE = "com.devlin.pmos.pmossync";
+	
+	  public static final String GET_ALL_PROPERTIES_URL = "http://pmos-restseerver.appspot.com/rest/m/saymore/";
+		
+	  // GCM config
+    public static final String GCM_SERVER_URL = "http://pmosnotifierserver.appspot.com/";
+    public static final String GCM_SENDER_ID = "639282159683"; // project ID from the APIs console
+    
+    // OAuth 2.0 related config
+    public static final String APP_NAME = "Your-App-Name";
+    public static final String API_KEY = "API_KEY"; // from the APIs console
+    public static final String CLIENT_ID = "0000000000000.apps.googleusercontent.com"; // from the APIs console
+
+}
