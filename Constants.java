ad7588e0dd5ff4b2fb2b9564a29e9645c9a4dde3
briandevlin <brian.devlin@gmail.com>
2014-01-26 08:19:20 -0500

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -11,11 +11,11 @@ public class Constants {
 	 */
 	public static final String AUTHTOKEN_TYPE = "com.devlin.pmos.pmossync";
 	
-	  public static final String GET_ALL_PROPERTIES_URL = "http://pmos-restseerver.appspot.com/rest/m/saymore/";
+	  public static final String GET_ALL_PROPERTIES_URL = "http://xxxxx.appspot.com/rest/m/saymore/";
 		
 	  // GCM config
-    public static final String GCM_SERVER_URL = "http://pmosnotifierserver.appspot.com/";
-    public static final String GCM_SENDER_ID = "639282159683"; // project ID from the APIs console
+    public static final String GCM_SERVER_URL = "http://xxxxx.appspot.com/";
+    public static final String GCM_SENDER_ID = "xxxxxxxxxxxxx"; // project ID from the APIs console
     
     // OAuth 2.0 related config
     public static final String APP_NAME = "Your-App-Name";
